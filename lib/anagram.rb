027f395f3f030dce95b4da7edb2642dba2286413
Erin Stansbury <ekstansbury@gmail.com>
2020-02-14 17:52:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+
+end
